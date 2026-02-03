9503b4e38841d50899b28e28ed255c1547f5753b
nperry02 <nate@atperry.com>
2026-02-03 11:39:41 -0500

    testing

--- blackjack.java
+++ blackjack.java
@@ -24,7 +24,7 @@ public class blackjack {
             if ("K".equals(rank) || "Q".equals(rank) || "J".equals(rank)) return 10;
             return Integer.parseInt(rank);
         }
-
+        // no One
         @Override
         public String toString() {
             return rank + suit;
